a4adee831bd28b2bffb0db140fc4fd22ddea3417
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2022-11-09 10:41:51 +0100

    changes

--- src/components/Dashboard/lessonTypes/learning/AudioLearningType.jsx
+++ src/components/Dashboard/lessonTypes/learning/AudioLearningType.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import Audio from "../../../../utilities/audio/Audio";
+import LessonButtonsController from "../../../../utilities/buttonsController/LessonButtonsController";
 
-const AudioLearningType = () => {
+const AudioLearningType = ({ info }) => {
   return (
     <div className="flex justify-center items-center py-5 h-[70vh]">
       <div className="">
-        <Audio />
+        <Audio audio={info} />
         <h1 className="my-20 text-4xl font-semibold block text-center">
-          Bonjour - Hello!
+          {info.word2} - {info.word1}
         </h1>
-        <div className="flex justify-between">
-          <button className="block self-center my-4 sm:mt-4 bg-secondary text-white py-2.5 px-10 rounded font-semibold hover:bg-shadowHard ease-in-out duration-300 transition-all">
-            Go Back
-          </button>
-          <button className="block self-center my-4 sm:mt-4 bg-secondary text-white py-2.5 px-10 rounded font-semibold hover:bg-shadowHard ease-in-out duration-300 transition-all">
-            Continue
-          </button>
-        </div>
+        <LessonButtonsController />
       </div>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import lessonReducer from "./lessonsStore/lessonSlices";
+import lessonsContainer from "./lessonsStore/lessonContainerSlice";
 
 export default configureStore({
   reducer: {
     lessons: lessonReducer,
+    lessonContainer: lessonsContainer,
   },
 });
